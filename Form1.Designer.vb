29f54d813accebdb9dd58b83f195e1f56101d552
Rupert <dublowduck@gmail.com>
2021-12-12 22:31:16 +0000

    form checks installation of IOT Link
    Form checks to see if IOT Link is installed. Also added settings file by accident (I can’t seem to remove it) and information about the program to the application page in the form's properties.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,71 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.labIotLink = New System.Windows.Forms.Label()
+        Me.labIotLinkInstalled = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'labIotLink
+        '
+        Me.labIotLink.AutoSize = True
+        Me.labIotLink.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.labIotLink.Location = New System.Drawing.Point(12, 40)
+        Me.labIotLink.Name = "labIotLink"
+        Me.labIotLink.Size = New System.Drawing.Size(105, 37)
+        Me.labIotLink.TabIndex = 0
+        Me.labIotLink.Text = "IOTLink"
+        '
+        'labIotLinkInstalled
+        '
+        Me.labIotLinkInstalled.AutoSize = True
+        Me.labIotLinkInstalled.BackColor = System.Drawing.SystemColors.Control
+        Me.labIotLinkInstalled.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.labIotLinkInstalled.Location = New System.Drawing.Point(151, 40)
+        Me.labIotLinkInstalled.Name = "labIotLinkInstalled"
+        Me.labIotLinkInstalled.Size = New System.Drawing.Size(130, 37)
+        Me.labIotLinkInstalled.TabIndex = 1
+        Me.labIotLinkInstalled.Text = "Unknown"
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(25, 124)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 2
+        Me.btnUpdate.Text = "Refresh"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(25, 299)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(623, 334)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.labIotLinkInstalled)
+        Me.Controls.Add(Me.labIotLink)
+        Me.Name = "Form1"
+        Me.Text = "IOTLink GUI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents labIotLink As Label
+    Friend WithEvents labIotLinkInstalled As Label
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnExit As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,37 @@
 ﻿Public Class Form1
+    'delcare variables
+    Dim blnIotLinkIsInstalled As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'blnIotLinkIsInstalled = False
+
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+
+        'check for IOTLink service file
+        If My.Computer.FileSystem.FileExists("C:\Program Files (x86)\IOTLink\IOTLinkService.exe") Then
+            blnIotLinkIsInstalled = True
+        Else
+            blnIotLinkIsInstalled = False
+        End If
+
+        If blnIotLinkIsInstalled = True Then
+            labIotLinkInstalled.Text = "Installed"
+            labIotLinkInstalled.BackColor = Color.Lime
+        ElseIf blnIotLinkIsInstalled = False Then
+            labIotLinkInstalled.Text = "Not Installed"
+            labIotLinkInstalled.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        blnIotLinkIsInstalled = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 
 End Class
